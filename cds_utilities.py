e811cb32fcb4f1cb60d1e8c118c869b263b76a63
Ksenia Zueva <ksezue@utu.fi>
2015-04-21 16:06:59 +0400

    Fix issue with mixed cases fro start codon

--- cds_utilities.py
+++ cds_utilities.py
@@ -1,7 +1,7 @@
 #Coding sequences utilities
 
 def test_if_seq_begins_start_codon(sequence) :
-        if sequence[0:3] == "ATG" :
-                return("correctStart")
-        else :
-                return("wrongStart")
+    if sequence[0:3].upper() == "ATG" :
+        return("correctStart")
+    else :
+        return("wrongStart")
